6bdb12cea18b39806ecd6957f92febac584a5e9b
netsys-it <danoplaskur@centrum.sk>
2016-12-07 19:38:04 +0100

    Update gladiatus.js init grease monkey

--- gladiatus.js
+++ gladiatus.js
@@ -1,1 +1,8 @@
-// Test
+// ==UserScript==
+// @name        Gladiatus bot
+// @namespace   https://github.com/netsys-it/Bots/blob/master/gladiatus.js
+// @description Bot for gladiatus game
+// @include     https://s15-sk.gladiatus.gameforge.com/*
+// @version     1
+// @grant       none
+// ==/UserScript==
