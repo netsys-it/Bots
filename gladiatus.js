81fe9321ac8c9f1d3e593024b4adcf2003e7ed0b
netsys-it <danoplaskur@centrum.sk>
2016-12-07 20:39:39 +0100

    Update gladiatus.js
    Adde basic functionality

--- gladiatus.js
+++ gladiatus.js
@@ -6,3 +6,45 @@
 // @version     1
 // @grant       none
 // ==/UserScript==
+var animalClass = document.getElementsByClassName('expedition_button');
+var animals = {"POTKAN":0,"RYS":1,"VLK":2,"MEDVED":3};
+var naVypravu = document.getElementsByClassName('cooldown_bar_text')[0];
+
+function getLvl(){
+ return parseInt(document.getElementById('header_values_level').innerHTML);
+}
+
+function chooseAnimal(){
+ if(getLvl() < 3){
+    return animals.POTKAN;
+ }
+ else if(getLvl() > 2 && getLvl() < 6){
+    return animals.RYS;
+ }
+ else if(getLvl() > 5 && getLvl() < 9){
+    return animals.VLK;
+ }
+ else{
+    return animals.MEDVED; 
+ }
+}
+
+function attackAnimal(){
+ //document.getElementsByClassName('cooldown_bar_link')[0].click();
+ animalClass[chooseAnimal()].click(); 
+}
+
+function checkTime(){
+  var end_timer;
+  var timer = setInterval(function(){
+    end_timer = document.getElementById('cooldown_bar_fill_expedition').innerHTML;
+    if(end_timer === "Na výpravu"){
+      clearInterval(timer);
+      attackAnimal();
+    }else{
+      console.log(end_timer);
+    }
+  },1000);
+}
+
+checkTime();
