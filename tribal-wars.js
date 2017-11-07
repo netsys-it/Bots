329a2012d7dd5eb52d5e815425b1a9589a294643
netsys-it <danoplaskur@centrum.sk>
2017-11-07 09:06:52 +0100

    added debug

--- tribal-wars.js
+++ tribal-wars.js
@@ -10,6 +10,7 @@
 
 (function() {
     'use strict';
+    var DEBUG = false
     var i, max;
     var iron = [];
     var send = document.getElementsByClassName('farm_icon farm_icon_a');
@@ -17,18 +18,40 @@
     var resources = document.getElementsByClassName('res');
     var rnd = Math.floor(Math.random() * 100) + 53;
 
-    if(!disabled){
+    if(DEBUG){
+        console.log("[VARIABLES]");
+        console.log("send: "+send);
+        console.log("disabled: "+disabled);
+        console.log("resources: "+resources);
+        console.log("rnd: "+rnd);
+        console.log("[END]");
+    }
+
+    if(!disabled[0]){
         for(i=5;i<resources.length;i+=3){
             iron.push(parseInt(resources[i].innerHTML));
+        }
+        if(DEBUG){
+            console.log("iron: "+iron);
         }
         for(i=0;i < iron.length; i++){
             max = Math.max.apply(Math, iron);
             send[iron.indexOf(max) + 1].click();
+            if(DEBUG){
+                console.log("max: "+iron);
+                console.log("send["+iron.indexOf(max) + 1+"]");
+            }
             location.reload();
         }
     }
     i=0;
     setInterval(function() {
+        if(DEBUG){
+            console.log("[setInterval]");
+            console.log("Time: "+i);
+            console.log("Random time: "+rnd);
+            console.log("[END]");
+        }
         if(i++ == rnd){
             i = 0;
             location.reload();
