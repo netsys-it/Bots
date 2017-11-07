5f85a17a6d2d45f0cb32c39546d56791d964d0f1
netsys-it <danoplaskur@centrum.sk>
2017-11-07 08:50:27 +0100

    Update tribal-wars

--- tribal-wars.js
+++ tribal-wars.js
@@ -13,11 +13,11 @@
     var i, max;
     var iron = [];
     var send = document.getElementsByClassName('farm_icon farm_icon_a');
-    var disabled = document.getElementsByClassName('farm_icon_disabled start_locked');
+    var disabled = document.getElementsByClassName('farm_icon_disabled start_locked farm_icon farm_icon_a');
     var resources = document.getElementsByClassName('res');
     var rnd = Math.floor(Math.random() * 100) + 53;
 
-    if(!disabled[1]){
+    if(!disabled){
         for(i=5;i<resources.length;i+=3){
             iron.push(parseInt(resources[i].innerHTML));
         }
